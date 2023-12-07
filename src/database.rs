fd4c5bf7b09861f98d888f16a94239738c0cddea
Alex Rutar <alex@rutar.org>
2023-12-08 00:28:02 +0100

    give constraint explicit name

--- src/database.rs
+++ src/database.rs
@@ -26,7 +26,10 @@ impl RecordDatabase {
             "CREATE TABLE CitationKeys (
                  name TEXT NOT NULL PRIMARY KEY,
                  record_key INTEGER,
-                 FOREIGN KEY(record_key) REFERENCES Records(key) ON DELETE CASCADE
+                 CONSTRAINT foreign_record_key
+                      FOREIGN KEY (record_key)
+                      REFERENCES Records(key)
+                      ON DELETE CASCADE
              )",
             (),
         )?;
