48722d17d89836e56df1d4908fed61fb1453ad25
Alex Rutar <alex@rutar.org>
2024-01-12 13:44:59 +0000

    Implement rudimentary `arxiv` source and other improvements (#10)
    Co-authored-by: Peiran Wu <15968905+wupr@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod api;
 mod database;
+mod entry;
 mod record;
 mod source;
 
-use api::*;
-use biblatex::Bibliography;
+use std::str::FromStr;
+
 use clap::Parser;
 use rusqlite::Result;
-use std::str::FromStr;
+
+use api::*;
+use entry::KeyedEntry;
 
 const DATABASE_FILE: &str = "cache.db";
 
@@ -24,30 +27,30 @@ fn main() {
     let mut record_db = create_test_db().unwrap();
 
     // Collect all entries which are not null
-    let valid_entries: Vec<Entry> = cli
+    let valid_entries: Vec<KeyedEntry> = cli
         .args
         .into_iter()
         // parse the source:sub_id arguments
         .filter_map(|input| match RecordId::from_str(&input) {
             Ok(record_id) => Some(record_id),
-            Err(error) => {
-                eprintln!("{}", error);
+            Err(err) => {
+                eprintln!("{}", err);
                 None
             }
         })
         .filter_map(|record_id| {
-            get_record(&mut record_db, record_id).map_or_else(
+            get_record(&mut record_db, &record_id).map_or_else(
                 // error retrieving record
                 |err| {
                     eprintln!("{}", err);
                     None
                 },
-                |record| {
-                    record.data.or_else(|| {
-                        // null record
-                        eprintln!("Warning: '{}' is a null record!", record.id);
+                |record| match record {
+                    Some(record) => Some(record.into()),
+                    None => {
+                        eprintln!("'{}' is a null record", record_id);
                         None
-                    })
+                    }
                 },
             )
         })
@@ -55,12 +58,13 @@ fn main() {
 
     // print biblatex strings
     for entry in valid_entries {
-        println!("{}", entry.to_biblatex_string())
+        println!("{}", entry)
     }
 }
 
 /// Populate the database with some records for testing purposes.
 fn create_test_db() -> Result<RecordDatabase, RecordError> {
+    use entry::{Entry, Fields};
     match std::fs::remove_file(DATABASE_FILE) {
         Ok(()) => {}
         Err(e) if e.kind() == std::io::ErrorKind::NotFound => {}
@@ -69,23 +73,31 @@ fn create_test_db() -> Result<RecordDatabase, RecordError> {
 
     let mut record_db = RecordDatabase::create(DATABASE_FILE)?;
 
-    let raw = "@article{test:000, author = {Rutar, Alex and Wu, Peiran}, title = {Autobib}}";
-    let bibliography = Bibliography::parse(raw).unwrap();
-    let entry = bibliography.get("test:000").unwrap();
+    let entry_1 = Entry {
+        entry_type: "code".to_string(),
+        fields: Fields {
+            author: Some("Rutar, Alex and Wu, Peiran".to_string()),
+            title: Some("Autobib".to_string()),
+            ..Fields::default()
+        },
+    };
     record_db.set_cached_data(&Record::new(
         RecordId::from_str("test:000").unwrap(),
-        Some(entry.clone()),
+        entry_1,
     ))?;
 
-    let raw2 = "@article{test:002, author = {Author, Test}, title = {A Sample Paper}}";
-    let bibliography2 = Bibliography::parse(raw2).unwrap();
-    let entry2 = bibliography2.get("test:002").unwrap();
+    let entry_2 = Entry {
+        entry_type: "article".to_string(),
+        fields: Fields {
+            author: Some("Author, Test".to_string()),
+            title: Some("A Sample Paper".to_string()),
+            ..Fields::default()
+        },
+    };
     record_db.set_cached_data(&Record::new(
         RecordId::from_str("test:002").unwrap(),
-        Some(entry2.clone()),
+        entry_2,
     ))?;
-
-    record_db.set_cached_data(&Record::new(RecordId::from_str("test:001").unwrap(), None))?;
 
     Ok(record_db)
 }
