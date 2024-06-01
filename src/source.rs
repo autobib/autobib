db705d2253ae6d7c578fb4444fc850cceea63b6f
Alex Rutar <alex@rutar.org>
2024-06-01 23:32:03 +0100

    Implement some missing sources and other source improvements
    - Implement JFM and DOI sources.
    - Improve the ARXIV source implementation.

--- src/source.rs
+++ src/source.rs
@@ -1,4 +1,5 @@
 pub mod arxiv;
+pub mod doi;
 pub mod jfm;
 pub mod zbl;
 pub mod zbmath;
@@ -20,6 +21,8 @@ pub type Validator = fn(&str) -> bool;
 pub fn lookup_source(source: &str) -> Either<Resolver, Referrer> {
     match source {
         "arxiv" => Either::Left(arxiv::get_record),
+        "doi" => Either::Left(doi::get_record),
+        "jfm" => Either::Right(jfm::get_canonical),
         "zbmath" => Either::Left(zbmath::get_record),
         "zbl" => Either::Right(zbl::get_canonical),
         // SAFETY: An invalid source should have been caught by a call to lookup_validator
@@ -31,6 +34,8 @@ pub fn lookup_source(source: &str) -> Either<Resolver, Referrer> {
 pub fn lookup_validator(source: &str) -> Option<Validator> {
     match source {
         "arxiv" => Some(arxiv::is_valid_id),
+        "doi" => Some(doi::is_valid_id),
+        "jfm" => Some(jfm::is_valid_id),
         "zbmath" => Some(zbmath::is_valid_id),
         "zbl" => Some(zbl::is_valid_id),
         _ => None,
--- src/source/arxiv.rs
+++ src/source/arxiv.rs
@@ -16,11 +16,11 @@ lazy_static! {
             r"(acc-phys|adap-org|alg-geom|ao-sci|astro-ph|astro-ph\.CO|astro-ph\.EP|astro-ph\.GA|astro-ph\.HE|astro-ph\.IM|astro-ph\.SR|atom-ph|bayes-an|chao-dyn|chem-ph|cmp-lg|comp-gas|cond-mat|cond-mat\.dis-nn|cond-mat\.mes-hall|cond-mat\.mtrl-sci|cond-mat\.other|cond-mat\.quant-gas|cond-mat\.soft|cond-mat\.stat-mech|cond-mat\.str-el|cond-mat\.supr-con|cs\.AI|cs\.AR|cs\.CC|cs\.CE|cs\.CG|cs\.CL|cs\.CR|cs\.CV|cs\.CY|cs\.DB|cs\.DC|cs\.DL|cs\.DM|cs\.DS|cs\.ET|cs\.FL|cs\.GL|cs\.GR|cs\.GT|cs\.HC|cs\.IR|cs\.IT|cs\.LG|cs\.LO|cs\.MA|cs\.MM|cs\.MS|cs\.NA|cs\.NE|cs\.NI|cs\.OH|cs\.OS|cs\.PF|cs\.PL|cs\.RO|cs\.SC|cs\.SD|cs\.SE|cs\.SI|cs\.SY|dg-ga|econ\.EM|econ\.GN|econ\.TH|eess\.AS|eess\.IV|eess\.SP|eess\.SY|funct-an|gr-qc|hep-ex|hep-lat|hep-ph|hep-th|math-ph|math\.AC|math\.AG|math\.AP|math\.AT|math\.CA|math\.CO|math\.CT|math\.CV|math\.DG|math\.DS|math\.FA|math\.GM|math\.GN|math\.GR|math\.GT|math\.HO|math\.IT|math\.KT|math\.LO|math\.MG|math\.MP|math\.NA|math\.NT|math\.OA|math\.OC|math\.PR|math\.QA|math\.RA|math\.RT|math\.SG|math\.SP|math\.ST|mtrl-th|nlin\.AO|nlin\.CD|nlin\.CG|nlin\.PS|nlin\.SI|nucl-ex|nucl-th|patt-sol|physics\.acc-ph|physics\.ao-ph|physics\.app-ph|physics\.atm-clus|physics\.atom-ph|physics\.bio-ph|physics\.chem-ph|physics\.class-ph|physics\.comp-ph|physics\.data-an|physics\.ed-ph|physics\.flu-dyn|physics\.gen-ph|physics\.geo-ph|physics\.hist-ph|physics\.ins-det|physics\.med-ph|physics\.optics|physics\.plasm-ph|physics\.pop-ph|physics\.soc-ph|physics\.space-ph|plasm-ph|q-alg|q-bio|q-bio\.BM|q-bio\.CB|q-bio\.GN|q-bio\.MN|q-bio\.NC|q-bio\.OT|q-bio\.PE|q-bio\.QM|q-bio\.SC|q-bio\.TO|q-fin\.CP|q-fin\.EC|q-fin\.GN|q-fin\.MF|q-fin\.PM|q-fin\.PR|q-fin\.RM|q-fin\.ST|q-fin\.TR|quant-ph|solv-int|stat\.AP|stat\.CO|stat\.ME|stat\.ML|stat\.OT|stat\.TH|supr-con)",
             r"/",
             // followed by the identifier
-            r"(([0-1][0-9])|(9[1-9]))(0[1-9]|1[0-2])([0-9]{3})(v[1-9][0-9]*)?",
+            r"(([0-1][0-9])|(9[1-9]))(0[1-9]|1[0-2])([0-9]{3})",
             r")",
             r"|",
-            // new style: YYMM.NNNN or YYMM.NNNNN or with version
-            r"([0-9][0-9](0[1-9]|1[0-2])[.][0-9]{4,5})(v[1-9][0-9]*)?",
+            // new style: YYMM.NNNN or YYMM.NNNNN
+            r"([0-9][0-9](0[1-9]|1[0-2])[.][0-9]{4,5})",
             r")$",
     )).unwrap();
 }
@@ -39,7 +39,7 @@ struct ArxivXMLEntry {
     title: String,
     author: Vec<ArxivXMLAuthor>,
     id: String,
-    updated: DateTime<Local>,
+    // updated: DateTime<Local>,
     published: DateTime<Local>,
 }
 
@@ -50,6 +50,11 @@ struct ArxivXMLAuthor {
 
 impl From<ArxivXMLEntry> for Entry {
     fn from(arxiv_xml: ArxivXMLEntry) -> Entry {
+        let arxiv_id = arxiv_xml
+            .id
+            .strip_prefix("http://arxiv.org/abs/")
+            .and_then(|s| s.rfind('v').and_then(|idx| Some(s[..idx].into())));
+
         Entry {
             entry_type: "preprint".into(),
             fields: Fields {
@@ -61,7 +66,8 @@ impl From<ArxivXMLEntry> for Entry {
                         .map(|auth| auth.name)
                         .join(" and "),
                 ),
-                year: Some(arxiv_xml.updated.year().to_string()),
+                year: Some(arxiv_xml.published.year().to_string()),
+                arxiv: arxiv_id,
                 ..Fields::default()
             },
         }
