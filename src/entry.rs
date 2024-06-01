ca6807a45a8acd4b8d8e4e70b223b997d4d66e51
Alex Rutar <alex@rutar.org>
2024-06-01 23:32:03 +0100

    Implement some missing sources and other source improvements
    - Implement JFM and DOI sources.
    - Improve the ARXIV source implementation.

--- src/entry.rs
+++ src/entry.rs
@@ -1,13 +1,60 @@
 use std::fmt::{Display, Formatter};
 
 use serde::{Deserialize, Serialize};
-use serde_aux::prelude::*;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct KeyedEntry {
     pub key: String,
     pub contents: Entry,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Entry {
+    pub entry_type: String,
+    pub fields: Fields,
+}
+
+impl Entry {
+    pub fn add_key<T: Into<String>>(self, key: T) -> KeyedEntry {
+        KeyedEntry {
+            key: key.into(),
+            contents: self,
+        }
+    }
+}
+
+/// A container for bibtex fields.
+///
+/// The aliases are required to handle zbmath.org bibtex field name formatting.
+/// This is a bit more robust if https://github.com/serde-rs/serde/pull/1902 or
+/// https://github.com/serde-rs/serde/pull/2161 is merged...
+///
+/// DO NOT USE `serde_aux::container_attributes::deserialize_struct_case_insensitive`.
+/// The problem is that `serde_aux` internally first deserializes to a map, and then deserializes
+/// into a struct. Since `serde_bibtex` uses skipped fields to ignore undefined macros,
+/// this can/will cause problems when deserializing.
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct Fields {
+    #[serde(alias = "Title")]
+    pub title: Option<String>,
+    #[serde(alias = "Author")]
+    pub author: Option<String>,
+    #[serde(alias = "Journal")]
+    pub journal: Option<String>,
+    #[serde(alias = "Volume")]
+    pub volume: Option<String>,
+    #[serde(alias = "Pages")]
+    pub pages: Option<String>,
+    #[serde(alias = "Year")]
+    pub year: Option<String>,
+    #[serde(alias = "DOI")]
+    pub doi: Option<String>,
+    pub arxiv: Option<String>,
+    #[serde(alias = "Language")]
+    pub language: Option<String>,
+}
+
+// TODO: once serde_bibtex::serialize is implemented, this can be deleted
 
 impl Display for KeyedEntry {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -22,52 +69,28 @@ impl Display for KeyedEntry {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Entry {
-    pub entry_type: String,
-    #[serde(deserialize_with = "deserialize_struct_case_insensitive")]
-    pub fields: Fields,
-}
-
-impl Entry {
-    pub fn add_key<T: Into<String>>(self, key: T) -> KeyedEntry {
-        KeyedEntry {
-            key: key.into(),
-            contents: self,
-        }
-    }
-}
-
-#[derive(Debug, Default, Serialize, Deserialize)]
-pub struct Fields {
-    pub title: Option<String>,
-    pub author: Option<String>,
-    pub journal: Option<String>,
-    pub volume: Option<String>,
-    pub pages: Option<String>,
-    pub year: Option<String>,
-}
-
-impl Fields {
-    fn write_biblatex_row(
-        f: &mut Formatter<'_>,
-        field_name: &str,
-        field_value: &Option<String>,
-    ) -> std::fmt::Result {
-        match field_value {
-            Some(field_value_string) => write!(f, "\n  {field_name} = {{{field_value_string}}},"),
-            None => Ok(()),
-        }
+fn write_biblatex_row(
+    f: &mut Formatter<'_>,
+    field_name: &str,
+    field_value: &Option<String>,
+) -> std::fmt::Result {
+    match field_value {
+        Some(field_value_string) => write!(f, "\n  {field_name} = {{{field_value_string}}},"),
+        None => Ok(()),
     }
 }
 
 impl Display for Fields {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        Self::write_biblatex_row(f, "title", &self.title)?;
-        Self::write_biblatex_row(f, "journal", &self.journal)?;
-        Self::write_biblatex_row(f, "volume", &self.volume)?;
-        Self::write_biblatex_row(f, "pages", &self.pages)?;
-        Self::write_biblatex_row(f, "year", &self.year)?;
-        Self::write_biblatex_row(f, "author", &self.author)
+        write_biblatex_row(f, "title", &self.title)?;
+        write_biblatex_row(f, "author", &self.author)?;
+        write_biblatex_row(f, "journal", &self.journal)?;
+        write_biblatex_row(f, "volume", &self.volume)?;
+        write_biblatex_row(f, "pages", &self.pages)?;
+        write_biblatex_row(f, "year", &self.year)?;
+        write_biblatex_row(f, "doi", &self.doi)?;
+        write_biblatex_row(f, "arxiv", &self.arxiv)?;
+        write_biblatex_row(f, "language", &self.language)?;
+        Ok(())
     }
 }
