de386cc3436f7dc1fa13b5e3a9bed323f7d169db
Peiran Wu <15968905+wupr@users.noreply.github.com>
2024-07-03 15:41:56 +0100

    Move `RecordDataError` to separate module

--- src/error/database.rs
+++ src/error/database.rs
@@ -1,22 +1,6 @@
 use thiserror::Error;
 
-#[derive(Error, Debug, PartialEq)]
-pub enum RecordDataError {
-    #[error("Key is not ASCII lowercase `[a-z]`.")]
-    KeyNotAsciiLowercase,
-    #[error("Key has invalid size {0}; must be at least 1 and at most `u8::MAX`.")]
-    KeyInvalidLength(usize),
-    #[error("Entry type is not ASCII lowercase `[a-z]`.")]
-    EntryTypeNotAsciiLowercase,
-    #[error("Entry type has invalid size {0}; must be at least 1 and at most `u8::MAX`.")]
-    EntryTypeInvalidLength(usize),
-    #[error("Value has invalid size {0}; must be at most `u16::MAX`.")]
-    ValueInvalidLength(usize),
-    #[error("Value does not contain balanced `{{ }}` brackets.")]
-    ValueNotBalanced,
-    #[error("Record data structure contains maximum number of entries.")]
-    RecordDataFull,
-}
+use super::RecordDataError;
 
 #[derive(Error, Debug)]
 pub enum DatabaseError {
