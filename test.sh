4731773e4f3476e87fa19d7829a4f07305558154
Alex Rutar <alex@rutar.org>
2024-10-13 23:30:53 +0300

    Delete unused files

--- test.sh
+++
@@ -1,6 +0,0 @@
-cargo build
-target/debug/autobib -v alias add alias zbl:1509.28005
-target/debug/autobib -v alias add relative alias
-target/debug/autobib -v get arxiv:1404.1016 arxiv:0313.1421 arxiv:2704.1943 zbmath:02568991 zbmath:2568991 zbmath:00000000 zbl:1509.28005 alias relative jfm:52.0047.07 doi:10.1090/proc/15307 arxiv:hep-th/9108001 arxiv:math/9201254
-target/debug/autobib -v alias delete relative
-target/debug/autobib -v get relative
