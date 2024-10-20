1392d7b9e13c04c2e8e5e9c916b94d2e8be0b243
Alex Rutar <alex@rutar.org>
2024-10-20 23:07:24 +0300

    Improve documentation and organization of the `record` module

--- src/record.rs
+++ src/record.rs
@@ -18,15 +18,20 @@ use crate::{
     HttpClient,
 };
 
-use private::Context;
+use private::NonEmptyStack;
 
+/// The funamdental record type.
 #[derive(Debug)]
 pub struct Record {
+    /// The original key.
     pub key: String,
+    /// The raw data.
     pub data: RawRecordData,
+    /// The canonical identifier.
     pub canonical: RemoteId,
 }
 
+/// The response type of [`get_record`].
 #[derive(Debug)]
 pub enum GetRecordResponse {
     /// The record exists.
@@ -37,16 +42,19 @@ pub enum GetRecordResponse {
     NullAlias(Alias),
 }
 
-impl GetRecordResponse {
-    /// Return `Some(Record)` if the record exists, and otherwise `None`.
-    pub fn ok(self) -> Option<Record> {
-        match self {
-            Self::Exists(record) => Some(record),
-            _ => None,
-        }
-    }
-}
-
+/// The response type of [`get_record_entry`].
+///
+/// If the record exists, the resulting [`RecordRow`] is guaranteed to be valid for the row corresponding
+/// to the [`Record`].
+///
+/// If the record does not exist, then the corresponding row is guaranteed to not exist.
+///
+/// The initial [`DatabaseEntry`] is passed back to the caller inside the enum. Note that this
+/// transaction *must* be committed in order for database changes to be in effect, regardless if
+/// the record exists or is null, since the null records are also cached inside the database.
+///
+/// This type can be converted to a usual [`GetRecordResponse`], at which point the internal
+/// transaction is automatically committed.
 #[derive(Debug)]
 pub enum GetRecordEntryResponse<'conn> {
     /// The record exists.
@@ -79,6 +87,9 @@ impl TryFrom<GetRecordEntryResponse<'_>> for GetRecordResponse {
 }
 
 /// Get the [`Record`] associated with a [`RecordId`], or [`None`] if the [`Record`] does not exist.
+///
+/// This is essentially a convenience method for the [`get_record_entry`] function, except the
+/// transaction is created and committed internally.
 pub fn get_record(
     db: &mut RecordDatabase,
     record_id: RecordId,
@@ -112,26 +123,31 @@ pub fn get_record_entry<'conn>(
         }
         DatabaseEntry::Missing(missing) => match record_id.resolve()? {
             Either::Left(alias) => Ok(GetRecordEntryResponse::NullAlias(alias, missing)),
-            Either::Right(remote_id) => remote_resolve(missing, Context::new(remote_id), client),
+            Either::Right(remote_id) => get_record_entry_remote_resolve(missing, remote_id, client),
         },
     }
 }
 
-/// Loop to resolve remote records.
-fn remote_resolve<'conn>(
+/// Resolve remote records inside a loop within a transaction.
+///
+/// At each intermediate stage, attempt to read any data possible from the database
+/// inside the transaction implicit in the [`MissingRecordRow`], and write any new data to the
+/// database.
+fn get_record_entry_remote_resolve<'conn>(
     mut missing: MissingRecordRow<'conn>,
-    mut context: Context,
+    remote_id: RemoteId,
     client: &HttpClient,
 ) -> Result<GetRecordEntryResponse<'conn>, Error> {
+    let mut history = NonEmptyStack::new(remote_id);
     loop {
-        let top = context.peek();
+        let top = history.peek();
 
         missing = match missing.apply(check_null(top))? {
             NullRecordsResponse::Found(_when) => {
-                // skip top element of Context since it is already cached
-                missing.apply(set_null(context.descend().skip(1)))?;
+                // skip top element of the stack since it is already cached
+                missing.apply(set_null(history.descend().skip(1)))?;
                 break Ok(GetRecordEntryResponse::NullRemoteId(
-                    context.into_top(),
+                    history.into_top(),
                     missing,
                 ));
             }
@@ -142,8 +158,8 @@ fn remote_resolve<'conn>(
                         Some(data) => {
                             let raw_record_data = (&data).into();
                             let row = missing.insert(&raw_record_data, top)?;
-                            row.apply(add_refs(context.descend()))?;
-                            let (bottom, top) = context.into_ends();
+                            row.apply(add_refs(history.descend()))?;
+                            let (bottom, top) = history.into_ends();
                             break Ok(GetRecordEntryResponse::Exists(
                                 Record {
                                     key: bottom.into(),
@@ -154,9 +170,9 @@ fn remote_resolve<'conn>(
                             ));
                         }
                         None => {
-                            missing.apply(set_null(context.descend()))?;
+                            missing.apply(set_null(history.descend()))?;
                             break Ok(GetRecordEntryResponse::NullRemoteId(
-                                context.into_bottom(),
+                                history.into_bottom(),
                                 missing,
                             ));
                         }
@@ -164,13 +180,13 @@ fn remote_resolve<'conn>(
                     Either::Right(referrer) => match referrer(top.sub_id(), client)? {
                         Some(new_remote_id) => match missing.reset(&new_remote_id)? {
                             DatabaseEntry::Exists(row) => {
-                                row.apply(add_refs(context.descend()))?;
+                                row.apply(add_refs(history.descend()))?;
                                 let RowData {
                                     data, canonical, ..
                                 } = row.apply(get_row_data)?;
                                 break Ok(GetRecordEntryResponse::Exists(
                                     Record {
-                                        key: context.into_bottom().into(),
+                                        key: history.into_bottom().into(),
                                         data,
                                         canonical,
                                     },
@@ -178,14 +194,14 @@ fn remote_resolve<'conn>(
                                 ));
                             }
                             DatabaseEntry::Missing(missing) => {
-                                context.push(new_remote_id);
+                                history.push(new_remote_id);
                                 missing
                             }
                         },
                         None => {
-                            missing.apply(set_null(context.descend()))?;
+                            missing.apply(set_null(history.descend()))?;
                             break Ok(GetRecordEntryResponse::NullRemoteId(
-                                context.into_bottom(),
+                                history.into_bottom(),
                                 missing,
                             ));
                         }
@@ -196,19 +212,25 @@ fn remote_resolve<'conn>(
     }
 }
 
+/// The result of obtaining a remote record, with no reference to a database.
 pub enum GetRemoteRecordResponse {
+    /// The remote record exists, and has the provided data.
     Exists(RawRecordData),
+    /// The remote record does not exist.
     Null(RemoteId),
 }
 
 /// Get the [`Record`] associated with a [`RemoteId`], or [`None`] if the [`Record`] does not exist.
+///
+/// This method does not involve any database reads or writes, and simply loops to obtain the
+/// remote record associated with a [`RemoteId`].
 pub fn get_remote_record(
     remote_id: RemoteId,
     client: &HttpClient,
 ) -> Result<GetRemoteRecordResponse, Error> {
-    let mut context = Context::new(remote_id);
+    let mut history = NonEmptyStack::new(remote_id);
     loop {
-        let top = context.peek();
+        let top = history.peek();
 
         info!("Resolving remote record for '{top}'");
         match lookup_provider(top.provider()) {
@@ -217,13 +239,13 @@ pub fn get_remote_record(
                     break Ok(GetRemoteRecordResponse::Exists(RawRecordData::from(&data)));
                 }
                 None => {
-                    break Ok(GetRemoteRecordResponse::Null(context.into_bottom()));
+                    break Ok(GetRemoteRecordResponse::Null(history.into_bottom()));
                 }
             },
             Either::Right(referrer) => match referrer(top.sub_id(), client)? {
-                Some(new_remote_id) => context.push(new_remote_id),
+                Some(new_remote_id) => history.push(new_remote_id),
                 None => {
-                    break Ok(GetRemoteRecordResponse::Null(context.into_bottom()));
+                    break Ok(GetRemoteRecordResponse::Null(history.into_bottom()));
                 }
             },
         }
@@ -231,62 +253,65 @@ pub fn get_remote_record(
 }
 
 mod private {
-    use super::RemoteId;
-
-    /// A [`Context`] is a non-empty stack holding [`RemoteId`].
+    /// A non-empty stack implementation.
     #[derive(Debug)]
-    pub struct Context(Vec<RemoteId>);
+    pub struct NonEmptyStack<T>(Vec<T>);
 
-    impl Context {
-        /// Construct a new [`Context`] with an initial element.
+    impl<T> NonEmptyStack<T> {
+        /// Construct a new [`NonEmptyStack`] with an initial element.
         #[inline]
-        pub fn new(first: RemoteId) -> Self {
+        pub fn new(first: T) -> Self {
             Self(vec![first])
         }
 
         /// Iterate from top to bottom
         #[inline]
-        pub fn descend(&self) -> impl Iterator<Item = &RemoteId> {
+        pub fn descend(&self) -> impl Iterator<Item = &T> {
             self.0.iter().rev()
         }
 
         /// Push a new element.
         #[inline]
-        pub fn push(&mut self, remote_id: RemoteId) {
+        pub fn push(&mut self, remote_id: T) {
             self.0.push(remote_id);
         }
 
         /// Get the top element.
         #[inline]
-        pub fn peek(&self) -> &RemoteId {
+        pub fn peek(&self) -> &T {
             // SAFETY: the internal vec is always non-empty
             unsafe { self.0.last().unwrap_unchecked() }
         }
 
-        /// Drop the context, extracting the bottom element.
+        /// Drop the stack, extracting the bottom element.
         #[inline]
-        pub fn into_bottom(mut self) -> RemoteId {
+        pub fn into_bottom(mut self) -> T {
             // SAFETY: the internal vec is always non-empty
             unsafe { self.0.drain(..).next().unwrap_unchecked() }
         }
 
-        /// Drop the context, extracting the top element.
+        /// Drop the stack, extracting the top element.
         #[inline]
-        pub fn into_top(self) -> RemoteId {
+        pub fn into_top(self) -> T {
             self.split_top().1
         }
 
-        /// Drop the context, extracting the top element.
+        /// Drop the stack, extracting the top element.
         #[inline]
-        pub fn split_top(mut self) -> (Vec<RemoteId>, RemoteId) {
+        pub fn split_top(mut self) -> (Vec<T>, T) {
             // SAFETY: the internal vec is always non-empty
             let top = unsafe { self.0.pop().unwrap_unchecked() };
             (self.0, top)
         }
+    }
 
-        /// Drop the context, extracting the top and bottom elements.
+    impl<T: Clone> NonEmptyStack<T> {
+        /// Drop the stack, extracting the top and bottom elements.
+        ///
+        /// Note that `T` must be [`Clone`], since it is possible that the stack has exactly one
+        /// element, so the top and the bottom elements are the same element.
         #[inline]
-        pub fn into_ends(mut self) -> (RemoteId, RemoteId) {
+        pub fn into_ends(mut self) -> (T, T) {
             unsafe {
                 if self.0.len() >= 2 {
                     // SAFETY: we just checked that the length is at least 2
