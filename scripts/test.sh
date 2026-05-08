d02f83e4a5ad6e04c3b95c6b83a0a2020bd00000
Peiran Wu <15968905+wupr@users.noreply.github.com>
2026-05-08 14:01:33 +0200

    Minor fixes in testing script

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,7 +9,7 @@ cd "$(dirname "${PROJECT_ROOT}")"
 
 REMOTES_FILE="tests/remotes.txt"
 CACHE_FORMAT_DEF="src/http/cache/format.rs"
-CACHE_HOME="${XDG_CACHE_HOME:=${HOME}/.cache/}"
+CACHE_HOME="${XDG_CACHE_HOME:=${HOME}/.cache}"
 CACHE_ROOT="${AUTOBIB_RESPONSE_CACHE_DIR:=${CACHE_HOME}/autobib}"
 
 # set up cache directory
@@ -20,14 +20,14 @@ CACHE_DIR="${CACHE_ROOT}/test-cache-${HASH}"
 export AUTOBIB_RESPONSE_CACHE_PATH="${CACHE_DIR}/responses.dat"
 
 if [[ ! -f "${AUTOBIB_RESPONSE_CACHE_PATH}" ]]; then
-    echo "Cache file not found. Generating cache file: ${AUTOBIB_RESPONSE_CACHE_PATH}"
-    
+    echo 2>&1 "Cache file not found. Generating cache file: ${AUTOBIB_RESPONSE_CACHE_PATH}"
+
     mkdir -p "${CACHE_DIR}"
 
     # Generate the cache file
     cargo run --locked --features write_response_cache -- -vv source --retrieve-only --ignore-null "${REMOTES_FILE}"
 else
-    echo "Cache file found: ${AUTOBIB_RESPONSE_CACHE_PATH}"
+    echo 2>&1 "Cache file found: ${AUTOBIB_RESPONSE_CACHE_PATH}"
 fi
 
 cargo test --locked --no-fail-fast --features read_response_cache -- "$@"
