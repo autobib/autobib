628b4bcc70d95fe876105ed22031a09b8e4e9a25
Alex Rutar <alex@rutar.org>
2026-07-29 22:09:52 +0200

    Add migration code

--- src/db/functions.rs
+++ src/db/functions.rs
@@ -1,9 +1,5 @@
-use autobib_entry::{
-    Archive,
-    data::{EntryData, archive},
-    v0, v1,
-};
-use rusqlite::{Connection, functions::FunctionFlags};
+use autobib_entry::{Archive, data::EntryData, v0};
+use rusqlite::{Connection, Error::UserFunctionError, functions::FunctionFlags};
 
 /// The available application functions.
 #[derive(Debug)]
@@ -14,9 +10,6 @@ pub enum AppFunction {
     ContainsField,
     ///`get_field(field: TEXT, data: BLOB) -> TEXT or NULL` returns the field value if it exists, or null.
     GetField,
-    ///`update_entry_data(data: BLOB) -> BLOB` accepts entry data in the `v0` format and returns
-    ///entry data in the `v1` format.
-    UpdateEntryData,
 }
 
 impl AppFunction {
@@ -26,7 +19,6 @@ impl AppFunction {
             Self::Regexp => "regexp",
             Self::ContainsField => "contains_field",
             Self::GetField => "get_field",
-            Self::UpdateEntryData => "update_entry_data",
         }
     }
 }
@@ -39,7 +31,6 @@ pub fn register_application_function(
         AppFunction::Regexp => add_regexp_function(conn),
         AppFunction::ContainsField => add_contains_field_function(conn),
         AppFunction::GetField => add_get_field_function(conn),
-        AppFunction::UpdateEntryData => add_update_entry_data_function(conn),
     }
 }
 
@@ -51,7 +42,9 @@ fn add_regexp_function(conn: &Connection) -> Result<(), rusqlite::Error> {
     conn.create_scalar_function(
         AppFunction::Regexp.name(),
         2,
-        FunctionFlags::SQLITE_UTF8 | FunctionFlags::SQLITE_DETERMINISTIC,
+        FunctionFlags::SQLITE_UTF8
+            | FunctionFlags::SQLITE_DETERMINISTIC
+            | FunctionFlags::SQLITE_INNOCUOUS,
         move |ctx| {
             assert_eq!(ctx.len(), 2, "called with unexpected number of arguments");
             let regexp: Arc<Regex> = ctx.get_or_create_aux(
@@ -64,7 +57,7 @@ fn add_regexp_function(conn: &Connection) -> Result<(), rusqlite::Error> {
                 let text = ctx
                     .get_raw(1)
                     .as_str()
-                    .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?;
+                    .map_err(|e| UserFunctionError(e.into()))?;
 
                 regexp.is_match(text)
             };
@@ -79,22 +72,24 @@ fn add_contains_field_function(conn: &Connection) -> Result<(), rusqlite::Error>
     conn.create_scalar_function(
         AppFunction::ContainsField.name(),
         2,
-        FunctionFlags::SQLITE_UTF8 | FunctionFlags::SQLITE_DETERMINISTIC,
+        FunctionFlags::SQLITE_UTF8
+            | FunctionFlags::SQLITE_DETERMINISTIC
+            | FunctionFlags::SQLITE_INNOCUOUS,
         move |ctx| {
             assert_eq!(ctx.len(), 2, "called with unexpected number of arguments");
             let field_name = ctx
                 .get_raw(1)
                 .as_str()
-                .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?;
+                .map_err(|e| UserFunctionError(e.into()))?;
 
             let is_match = {
                 let data = ctx
                     .get_raw(0)
                     .as_blob()
-                    .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?;
+                    .map_err(|e| UserFunctionError(e.into()))?;
 
                 v0::ArchivedEntryData::access(data)
-                    .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?
+                    .map_err(|e| UserFunctionError(e.into()))?
                     .contains_field(field_name)
             };
 
@@ -108,59 +103,29 @@ fn add_get_field_function(conn: &Connection) -> Result<(), rusqlite::Error> {
     conn.create_scalar_function(
         AppFunction::GetField.name(),
         2,
-        FunctionFlags::SQLITE_UTF8 | FunctionFlags::SQLITE_DETERMINISTIC,
+        FunctionFlags::SQLITE_UTF8
+            | FunctionFlags::SQLITE_DETERMINISTIC
+            | FunctionFlags::SQLITE_INNOCUOUS,
         move |ctx| {
             assert_eq!(ctx.len(), 2, "called with unexpected number of arguments");
             let field_name = ctx
                 .get_raw(1)
                 .as_str()
-                .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?;
+                .map_err(|e| UserFunctionError(e.into()))?;
 
             let field_value = {
                 let data = ctx
                     .get_raw(0)
                     .as_blob()
-                    .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?;
+                    .map_err(|e| UserFunctionError(e.into()))?;
 
                 v0::ArchivedEntryData::access(data)
-                    .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?
+                    .map_err(|e| UserFunctionError(e.into()))?
                     .get_field_str(field_name)
             };
 
             // this has to be 'static
             Ok(field_value.map(ToOwned::to_owned))
-        },
-    )
-}
-
-/// Register `update_entry_data` callback.
-fn add_update_entry_data_function(conn: &Connection) -> Result<(), rusqlite::Error> {
-    conn.create_scalar_function(
-        AppFunction::UpdateEntryData.name(),
-        1,
-        FunctionFlags::SQLITE_DETERMINISTIC,
-        move |ctx| {
-            assert_eq!(ctx.len(), 1, "called with unexpected number of arguments");
-            let data = ctx
-                .get_raw(0)
-                .as_blob()
-                .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?;
-
-            let new_data = match data.first() {
-                Some(0) => {
-                    let v0_data = v0::ArchivedEntryData::access(data)
-                        .map_err(|e| rusqlite::Error::UserFunctionError(e.into()))?;
-                    archive::<v1::ArchivedEntryData, _>(&v0_data)
-                }
-                Some(1) => data.into(),
-                _ => {
-                    return Err(rusqlite::Error::UserFunctionError(
-                        autobib_entry::error::AccessError::Unrecognized.into(),
-                    ));
-                }
-            };
-
-            Ok(new_data)
         },
     )
 }
